481d22223041f625fdf85a8833964f4a53ce9645
Lee Doughty <leedoughtydesign@gmail.com>
2018-11-15 12:08:01 +0000

    Add sphere object and delete mousemove even listener in three scene

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
 let container;
 let camera, scene, renderer;
-let mesh, lightMesh, torus;
+let mesh, lightMesh, torus, sphere;
+let toruses = [];
 let spheres = [];
 let directionalLight, pointLight;
-let mouseX = 0;
-let mouseY = 0;
 let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
 
-// let texture = new THREE.TextureLoader().load( "textures/water.jpg" );
-// texture.wrapS = THREE.RepeatWrapping;
-// texture.wrapT = THREE.RepeatWrapping;
-// texture.repeat.set( 4, 4 );
-
-// document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 init();
 animate();
 function init() {
@@ -25,24 +18,40 @@ function init() {
 	scene.background = new THREE.CubeTextureLoader()
 		.setPath( './images/index/' )
 		.load( [ 'px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png' ] );
-	let torus = new THREE.TorusGeometry( 100, 30, 160, 100 );
-	let material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: scene.background } );
-	for ( let i = 0; i < 100; i ++ ) {
+
+// Geometries
+let torus = new THREE.TorusGeometry( 100, 30, 160, 100 );
+let sphere = new THREE.SphereGeometry(5,32,32);
+
+// toruses
+let material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: scene.background } );
+	for ( let i = 0; i < 20; i ++ ) {
 		let mesh = new THREE.Mesh( torus, material );
 		mesh.position.x = Math.random() * 10000 - 5000;
 		mesh.position.y = Math.random() * 10000 - 5000;
 		mesh.position.z = Math.random() * 10000 - 5000;
 		mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 3 + 1;
 		scene.add( mesh );
-		spheres.push( mesh );
-	}
-	//
-	renderer = new THREE.WebGLRenderer();
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	container.appendChild( renderer.domElement );
-	//
-	window.addEventListener( 'resize', onWindowResize, false );
+		toruses.push( mesh );
+}
+
+//spheres
+for ( let i = 0; i < 20; i ++ ) {
+	let sphereMesh = new THREE.Mesh( sphere, material );
+	sphereMesh.position.x = Math.random() * 10000 - 5000;
+	sphereMesh.position.y = Math.random() * 10000 - 5000;
+	sphereMesh.position.z = Math.random() * 10000 - 5000;
+	sphereMesh.scale.x = sphereMesh.scale.y = sphereMesh.scale.z = Math.random() * 3 + 1;
+	scene.add( sphereMesh );
+	spheres.push( sphereMesh );
+}
+
+
+renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+container.appendChild( renderer.domElement );
+window.addEventListener( 'resize', onWindowResize, false );
 }
 
 function onWindowResize() {
@@ -65,13 +74,10 @@ function animate() {
 
 function render() {
 	let timer = 0.0001 * Date.now();
-	for ( let i = 0, il = spheres.length; i < il; i ++ ) {
-		let sphere = spheres[ i ];
-		sphere.position.x = 5000 * Math.cos( timer + i );
-		sphere.position.y = 5000 * Math.sin( timer + i * 1.1 );
+	for ( let i = 0, il = toruses.length; i < il; i ++ ) {
+		let torus = toruses[ i ];
+		torus.position.x = 5000 * Math.cos( timer + i );
+		torus.position.y = 5000 * Math.sin( timer + i * 1.1 );
 	}
-	camera.position.x += ( mouseX - camera.position.x ) * .05;
-	camera.position.y += ( - mouseY - camera.position.y ) * .05;
-	camera.lookAt( scene.position );
 	renderer.render( scene, camera );
 }
